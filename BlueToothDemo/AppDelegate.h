12166aede3dde29f9b42aaf4e185343f51af47d2
Morris <morris@morrisdemacbook-pro.local>
2016-02-05 19:38:40 +0800

    Initial Commit

---
+++ BlueToothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueToothDemo
+//
+//  Created by Morris on 2/5/16.
+//  Copyright © 2016 wola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueToothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueToothDemo
+//
+//  Created by Morris on 2/5/16.
+//  Copyright © 2016 wola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
